1f27d466cde306724696bcedca3ae75d3b9af25a
0xHJK <HJKdev+GitHub@gmail.com>
2016-10-09 15:21:55 +0800

    change name

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --name address-nginx -p 8000:80 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro -v $(pwd)/log:/root/log -d nginx
+docker run --name remote_addr_nginx -p 8000:80 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro -v $(pwd)/log:/root/log -d nginx
