83e54252cdc857cb0167440c5125f9e712479b0d
0xHJK <HJKdev+GitHub@gmail.com>
2017-01-17 18:56:59 +0800

    support visit domain directly

---
+++ rock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DOMAIN='ip.github.tk'
+
+# see more https://github.com/jwilder/nginx-proxy
+
+docker run -d --name return_ip -p 10086:80 -e VIRTUAL_HOST=${DOMAIN} -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro -v $(pwd)/log:/root/log nginx
