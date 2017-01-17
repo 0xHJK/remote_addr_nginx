45b2ee95a8546ae7844779be16263530dfe602cb
0xHJK <HJKdev+GitHub@gmail.com>
2017-01-17 20:17:21 +0800

    config fix

--- rock.sh
+++ rock.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-DOMAIN='test.local.com'
+DOMAIN=''
 
 # see more https://github.com/jwilder/nginx-proxy
 
-docker run -d --name return_ip -p 10086:80 -e VIRTUAL_HOST=${DOMAIN} -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro -v $(pwd)/log:/root/log nginx
+# docker run -d --name return_ip -p 10086:80 -e VIRTUAL_HOST=${DOMAIN} -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro -v $(pwd)/log:/root/log nginx
+
+docker run -d --name return_ip -p 10086:80 -v $(pwd)/nginx.conf:/etc/nginx/nginx.conf:ro -v $(pwd)/log:/root/log nginx
